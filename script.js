3c9fe78fe64e951e220996043301a61f44301c36
thalita15 <106327210+thalita15@users.noreply.github.com>
2024-09-04 08:33:15 -0300

    .

---
+++ script.js
@@ -0,0 +1,3 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
